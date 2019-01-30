dd56b1f1744fd94879fb57349778ffc2a75dda5e
Charlotte <charlotte.groder@gmail.com>
2019-01-30 16:01:51 +0100

    Endringer i flashcards

--- src/components/flashcards/CreateFlashcards.js
+++ src/components/flashcards/CreateFlashcards.js
@@ -1,29 +1,53 @@
 import React, {Component } from 'react'
-import { connect } from 'react-redux'
 import Flippy, {FrontSide, BackSide} from "react-flippy"
 
 class CreateFlashcards extends Component {
     state ={
-
     }
     render() {
         return(
-            <div className = "container">
-            <Flippy 
-                flipOnHover = {false}
-                flipOnClick = {true}
-                flipDirection = "horizontal"
-                ref = {(r) => this.flippy = r}
-                style = {{width:"200px", height:"200px"}}>
-                <FrontSide style={{backgroundColor: "#a8cdbb"}}> 
-                    <i class="material-icons">cached</i>
-                </FrontSide>
-                <BackSide style = {{backGroundColor: "#229059"}}> 
-                    <p>Kanji</p>
-                </BackSide>
-            </Flippy>
-        </div>
-        )
+            <div class="container">
+                <div className="s12 m3 l4">
+                    <Flippy
+                        flipOnClick = {true}
+                        flipDirection = "horizontal"
+                        ref = {(r) => this.flippy = r}
+                        style = {{width:"300px", height:"400px"}}>
+                        <FrontSide 
+                        style={{backgroundColor: "#e0e5e2"}} >
+                            <div className="center-align">
+                                <h1>漢</h1>
+                                <h6>Click me!</h6>
+                            </div> 
+                        </FrontSide>
+                        <BackSide
+                        style={{ backgroundColor: '#e0e5e2'}}>
+                        
+                            <div className="center-align">
+                                    <h1>漢</h1>
+                                    <h6>town</h6>
+                            </div> 
+
+                            <div className="s12 m3 l4">
+                                <p>Radikal 1: Ricepaddy</p>
+                                <p>Radikal 2: Street</p>
+                            </div>
+                            <div class="row">
+                            <form class="col s12">
+                            <div class="row">
+                                <div class="input-field col s12">
+                                <textarea id="textarea1" class="materialize-textarea"></textarea>
+                                <label for="textarea1">Husketekst</label>
+                                </div>
+                            </div>
+                            </form>
+                        </div>
+                        </BackSide>
+                    </Flippy>
+                </div>
+            </div>
+           
+        )   
     }
 } 
 
