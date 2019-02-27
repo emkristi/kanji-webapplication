d3c427998e4c7ddd316bfd57d3be16c22514370b
Charlotte <charlotte.groder@gmail.com>
2019-02-27 18:03:03 +0100

    facebookLogin

--- src/config/fbConfig.js
+++ src/config/fbConfig.js
@@ -1,10 +1,10 @@
 import firebase from 'firebase/app'
 import 'firebase/firestore'
 import 'firebase/auth'
+//import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth'
 
 // Initialize Firebase
 var config = {
-
     apiKey: "AIzaSyCDQX_UAYLg44oOF40BGMT14Uk4prHJ5Hk",
     authDomain: "kanjiapp-8f121.firebaseapp.com",
     databaseURL: "https://kanjiapp-8f121.firebaseio.com",
@@ -15,5 +15,5 @@ var config = {
 
   firebase.initializeApp(config);
   firebase.firestore().settings({ timestampsInSnapshots: true })
-
+  //provider = new firebase.auth.FacebookAuthProvider();
   export default firebase
