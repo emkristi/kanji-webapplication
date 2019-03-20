9c0ad5abac733d5d8680b10163ee775325952ef9
Charlotte <charlotte.groder@gmail.com>
2019-03-20 23:03:17 +0100

    nesten fungerende radikal-funksjon!

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -7,27 +7,28 @@ import FlashcardInfo from '../flashcards/FlashcardInfo';
 
 class Dashboard extends Component {
 
-// intern this.state -> this.setState for å sette den
-// this.state.currentCard for å bruke
-  constructor(props){
+  // intern this.state -> this.setState for å sette den
+  // this.state.currentCard for å bruke
+  constructor(props) {
     super(props);
     this.state = {
-      currentCard: 0, 
-      hCards:[],        
-      eCards:[]   
+      currentCard: 0,
+      hCards: [],
+      eCards: []
     };
   }
 
   test = () => {
-    if(this.props.flashcards){
+    if (this.props.flashcards) {
 
-    } 
+    }
   };
 
-  handleHard = (e) =>{
+  handleHard = (e) => {
     this.setState((state) => {
-      return { 
-        hCards: [...state.hCards, this.state.currentCard]} 
+      return {
+        hCards: [...state.hCards, this.state.currentCard]
+      }
     });
     this.handleClick();
   }
@@ -36,97 +37,147 @@ class Dashboard extends Component {
     this.setState((state) => {
       return {
         eCards: [...state.eCards, this.state.currentCard]
-        }              
+      }
     });
     this.handleClick();
   }
 
-  handleClick = (e) =>  {
-   const { flashcards } = this.props;
-   const { users } = this.props;  
-   const {auth} = this.props;
-   console.log("filter", flashcards.filter(item => item.radicals === "")); //sorterer kanjine til å bare være radikaler.
-   
-   
-   let i = this.state.currentCard;
-   console.log("Current card", i);
-   console.log("ecards length: ", this.state.eCards); 
-   console.log("flashcards length: ", flashcards); 
-   
-   for(let i=0; i<flashcards.length; i++){
-    console.log("tegn i flashcard",flashcards[i].kanji);
-   }
-   
-   console.log("users", users);
+  handleClick = (e) => {
+    const { flashcards } = this.props;
+    const { users } = this.props;
+    const { auth } = this.props;
+    console.log("filter", flashcards.filter(item => item.radicals === "")); //sorterer kanjine til å bare være radikaler.
 
 
-   //let a= users[i].flashcards[i].radicals; //henter ut radikalene fra en bruker
-   //får opp et kanji fra database
-   // 1) sjekk om radikalet er gått igjennom, om den finnes i brukerens flashcardarray
-   // 2) Hvis i array => sjekk om andre radikalet er i database
-   // hvis radikal 1 ikke er i database: Da skal dette radikalet komme opp istedet for kanjien. Legg så i flashcardarray. 
-  
+    let i = this.state.currentCard;
+    console.log("Current card", i);
 
-/*if (users[i].id === "6kSNDh8XyDfpyaGuQhtfYqzbp4o1"){
-  console.log("id=", users[i].id)
-  let a= users[i].flashcards[i].radicals; //henter ut radikalene fra en bruker
-  console.log("radikalene gitt bruker:",a);
-
-}*/
-
- /* for (let i=0; i<users.length; i++){
-    if(users[i].id === auth.uid){
-      console.log("uid finnes i id")
-      console.log("usersid", users[i].id);
-      console.log("uid", auth.uid);
-      //IKKE VIS ALLE USERS??!?!
+    for (let i = 0; i < flashcards.length; i++) {
+      console.log("tegn i flashcard", flashcards[i].kanji);
     }
-  }*/
 
-  /*
-  if(users[i].id !== auth.uid){
-    console.log("uid",auth.uid);
-    console.log(users[i].id);
-  } */
-  
+    console.log("users", users);
 
-  if(this.state.eCards.length+1 === flashcards.length){
+
+    //let a= users[i].flashcards[i].radicals; //henter ut radikalene fra en bruker
+    //får opp et kanji fra database
+    // 1) sjekk om radikalet er gått igjennom, om den finnes i brukerens flashcardarray
+    // 2) Hvis i array => sjekk om andre radikalet er i database
+    // hvis radikal 1 ikke er i database: Da skal dette radikalet komme opp istedet for kanjien. Legg så i flashcardarray. 
+
+
+    /*if (users[i].id === "6kSNDh8XyDfpyaGuQhtfYqzbp4o1"){
+      console.log("id=", users[i].id)
+      let a= users[i].flashcards[i].radicals; //henter ut radikalene fra en bruker
+      console.log("radikalene gitt bruker:",a);
+    
+    }*/
+
+    /* for (let i=0; i<users.length; i++){
+       if(users[i].id === auth.uid){
+         console.log("uid finnes i id")
+         console.log("usersid", users[i].id);
+         console.log("uid", auth.uid);
+         //IKKE VIS ALLE USERS??!?!
+       }
+     }*/
+
+    /*
+    if(users[i].id !== auth.uid){
+      console.log("uid",auth.uid);
+      console.log(users[i].id);
+    } */
+
+
+    //Sjekke flashcardtabellen om det finnes radikaler i tabellen. Hvis ja: Gå igjennom denne før kanjien vises.
+    if (this.state.eCards.length + 1 === flashcards.length) {
       console.log("Du blir sendt ut av deck");
-      window.location.href='/decks';
+      window.location.href = '/decks';
       return;
-  }
-    let currentNumber = (Math.round(Math.random() * (flashcards.length-1)));
-
-    while((this.state.eCards.includes(currentNumber) || currentNumber === this.state.currentCard)){
-      currentNumber = (Math.round(Math.random() * (flashcards.length-1)));
-      console.log("Nå skal random funksjonen kalles");
-      console.log("currentnumber", currentNumber);
-
-      /*
-      if(users[i].flashcards[i].radicals[i].includes("nASldc6p15eNon4U40Wa")){
-        console.log("radikal finnes");
-      }*/
-
-      for(let i=0; i<users.length; i++){
-        console.log("users[i]", users[i]);
-        if(users[i].flashcards != null) {
-          users[i].flashcards.forEach(flashCard => {
-            console.log(flashCard.radicals);
-          });
-         }
-        }
     }
-    this.setState({
 
+    let user = users[0];
+    let currentNumber;// = (Math.round(Math.random() * (flashcards.length - 1)));
+
+    let temp = true;
+    let element = "";
+    for (let x = 0; x < user.flashcards.length; x++) {
+      element += user.flashcards[x].kanji + ", ";
+    }
+    let element2 = "";
+    for (let x = 0; x < flashcards.length; x++) {
+      element2 += flashcards[x].kanji + ", ";
+    }
+    console.log(element, user.flashcards.length);
+    console.log(element2, flashcards.length);
+
+
+    if (user.flashcards.length === flashcards.length + 1) {
+      alert("ingen flere kort som ikke er lest");
+      temp = false;
+    }
+
+    restartWhile:
+    while (temp) {
+      currentNumber = (Math.round(Math.random() * (flashcards.length - 1)));
+
+      console.log(user.flashcards.includes(flashcards[currentNumber]), flashcards[currentNumber], user.flashcards);
+
+      if (user.flashcards.includes(flashcards[currentNumber]) || this.state.eCards.includes(currentNumber) || currentNumber === this.state.currentCard) {
+        //if (user.flashcards.includes(flashcards[currentNumber]) || user.flashcards[user.flashcards.length - 1] === flashcards[currentNumber]) {
+        continue restartWhile;
+      } else {
+        let tempRadicals = flashcards[currentNumber].radicals;
+        if (tempRadicals.length === 0) temp = false;
+        else {
+          for (let i = 0; i < tempRadicals.length; i++) {
+            let tempCheck = false;
+            for (let j = 0; j < user.flashcards.length; j++) {
+              if (tempRadicals[i] === user.flashcards[j].kanji) {
+                tempCheck = true;
+              }
+            }
+            if (!tempCheck) {
+              continue restartWhile;
+            }
+            tempCheck = false;
+          }
+          temp = false;
+        }
+      }
+    }
+
+    user.flashcards.push(flashcards[currentNumber]);
+
+    /* while ((this.state.eCards.includes(currentNumber) || currentNumber === this.state.currentCard)) {
+       currentNumber = (Math.round(Math.random() * (flashcards.length - 1)));
+       console.log("Nå skal random funksjonen kalles");
+       console.log("currentnumber", currentNumber);
+ 
+ 
+       /*
+       if(users[i].flashcards[i].radicals[i].includes("nASldc6p15eNon4U40Wa")){
+         console.log("radikal finnes");
+       }*/
+    /* for (let i = 0; i < users[].length; i++) {
+       console.log("users[i]", users[i]);
+       if (users[i].flashcards != null) {
+         users[i].flashcards.forEach(flashCard => {
+           console.log(flashCard.radicals);
+         });
+       }
+     }*/
+    //}
+    this.setState({
       currentCard: currentNumber
     });
   }
 
-  insertContentFlashcard (){
+  insertContentFlashcard() {
     //Code to insert content to the flashcard
     var card = document.querySelector('.card');
     console.log("insertContentFlashcard(1)");
-    card.addEventListener( 'click', function() {
+    card.addEventListener('click', function () {
       card.classList.toggle('is-flipped');
       console.log("insertContentFlashcard()");
     });
@@ -136,34 +187,34 @@ class Dashboard extends Component {
     // destructuring to get the flashcards. this grabs the flashcards object off the props. we can now pass the flashcards down as a prop into the ListOfFlashcards component
     const { flashcards } = this.props;
     const { auth } = this.props;
-    
+
     this.test();
 
-  /*
-
-    console.log(currentCard, "currentcard");
-    if (!auth.uid) return <Redirect to='/signin' />
-    return (
-      <div class="card blue-grey darken-1">
-        <div class="card" onClick={this.insertContentFlashcard}>
-          <div class="card__face card__face--front">
-            <div class="container">
-              {flashcards && 
-                  <FlashcardInfo flashcard={flashcards[this.state.currentCard]} />
-              } 
+    /*
+  
+      console.log(currentCard, "currentcard");
+      if (!auth.uid) return <Redirect to='/signin' />
+      return (
+        <div class="card blue-grey darken-1">
+          <div class="card" onClick={this.insertContentFlashcard}>
+            <div class="card__face card__face--front">
+              <div class="container">
+                {flashcards && 
+                    <FlashcardInfo flashcard={flashcards[this.state.currentCard]} />
+                } 
+              </div>
             </div>
+            <div class="card__face card__face--back">Engelsk + radikal</div>
           </div>
-          <div class="card__face card__face--back">Engelsk + radikal</div>
-        </div>
-        <p>Click card to flip</p>
-        <button onClick={this.handleClick}>Next</button>
-
-*/
+          <p>Click card to flip</p>
+          <button onClick={this.handleClick}>Next</button>
+  
+  */
     if (!auth.uid) return <Redirect to='/signin' />
     return (
       <div className="dashboard container">
-        { flashcards && 
-            <FlashcardInfo flashcard={flashcards[this.state.currentCard]}/> 
+        {flashcards &&
+          <FlashcardInfo flashcard={flashcards[this.state.currentCard]} />
         }
         <div>
           <button onClick={this.handleHard} id="Hard">Hard</button>
@@ -197,14 +248,16 @@ export default compose(
    * connect() is a function which returns a higher order component to take in the Dashboard.
    * We need to map our state from the store to the props in the dashboard component -> mapStateToProps.
    */
-  
-  connect(mapStateToProps), 
+
+  connect(mapStateToProps),
   firestoreConnect([ // we use firestoreConnect to tell which collection we want to connect to. takes in an array that contains a series of objects
-    { collection: 'flashcards' 
+    {
+      collection: 'flashcards'
     } // contains one object that says which collection we want to conenct to..
   ]),
   firestoreConnect([ // we use firestoreConnect to tell which collection we want to connect to. takes in an array that contains a series of objects
-    { collection: 'users' 
+    {
+      collection: 'users' //fikse dette!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
     } // contains one object that says which collection we want to conenct to..
   ])
   /**
