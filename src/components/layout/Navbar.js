0767bcca0462878cead8f0c50e8f2e7cb7dd4856
Eline <emkristi@stud.ntnu.no>
2019-05-05 14:41:22 +0200

    navbar warning fixed

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,43 +1,41 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
-import SignedInLinks from './SignedInLinks'
-import SignedOutLinks from './SignedOutLinks'
 import { connect } from 'react-redux'
-import { Dropdown, Button, Divider } from 'react-materialize'
+import { Dropdown } from 'react-materialize'
 import { signOut } from '../../store/actions/authActions';
 
 const Navbar = (props) => {
-    const { auth, profile } = props;
+    const { auth} = props;
 
     return (
         <div>
-            {!(window.location.pathname === "/start") && 
+        {!(window.location.pathname === "/start") && 
             <div>
-            <nav class="transparent z-depth-0">
-                <div class="nav-wrapper">
-                    <a href="/"><img class="navlogo" src="img/LOGOLITENNY.jpg" width="60rem" alt="logo" /></a>
-                    <ul class="right">
-                        {(auth.uid) &&
-                            <Dropdown trigger={<a class="brand-logo"><i class="material-icons md-36">menu</i></a>} options={{coverTrigger: false}}>
-                                <a className="navbarlinks" href='/howto'>How to play</a>
-                                <a className="navbarlinks" href='/about'>Credits</a>
-                                <a className="navbarlinks" href='/start' onClick={props.signOut}>Log out</a>
-                            </Dropdown>
-                        }
+                <nav className="transparent z-depth-0">
+                    <div className="nav-wrapper">
+                        <a href="/"><img className="navlogo" src="img/LOGOLITENNY.jpg" width="60rem" alt="logo" /></a>
+                        
+                        <ul className="right">
+                            {(auth.uid) &&
+                                <Dropdown trigger={<a className="brand-logo right"><i className="right material-icons md-36">menu</i></a>} options={{coverTrigger: false}}>
+                                    <a className="navbarlinks" href='/howto'>How to play</a>
+                                    <a className="navbarlinks" href='/about'>Credits</a>
+                                    <a className="navbarlinks" href='/start' onClick={props.signOut}>Log out</a>
+                                </Dropdown>
+                            }
 
-                        {(!auth.uid) &&
-                            <Dropdown trigger={<a class="brand-logo"><i class="material-icons md-36">menu</i></a>} options={{coverTrigger: false}}>
-                                <a className="navbarlinks" href='/signup'>Sign up</a>
-                                <a className="navbarlinks" href='/signin'>Sign in</a>
-                                <a className="navbarlinks" href='/howto'>How to play</a>
-                                <a className="navbarlinks" href='/about'>Credits</a>
-                            </Dropdown>
-                        }
-                    </ul>
-                </div>
-            </nav>
+                            {(!auth.uid) &&
+                                <Dropdown trigger={<a className="brand-logo right"><i className="material-icons md-36">menu</i></a>} options={{coverTrigger: false}}>
+                                    <a className="navbarlinks" href='/signup'>Sign up</a>
+                                    <a className="navbarlinks" href='/signin'>Sign in</a>
+                                    <a className="navbarlinks" href='/howto'>How to play</a>
+                                    <a className="navbarlinks" href='/about'>Credits</a>
+                                </Dropdown>
+                            }
+                        </ul>
+                    </div>
+                </nav>
             </div>
-            }
+        }
         </div>       
     )
 }
